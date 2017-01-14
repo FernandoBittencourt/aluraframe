95a11b165fdfdc6b95f23fd12652199a7cffadec
Fernando Figueiredo Drummond Bittencourt <fernandodbittencourt@hotmail.com>
2017-01-14 12:10:04 -0200

    Isolando a logica de criação e limpando o formulario ao criar uma negociação.

--- client/js/app/controllers/NegociacaoController.js
+++ client/js/app/controllers/NegociacaoController.js
@@ -12,16 +12,23 @@ class NegociacaoController{
 	adiciona(event) {
         event.preventDefault();
 
-        let data = DateHelper.textoParaData(this._inputData.value);
+        this._listaNegociacoes.adiciona(_criaNegociacao());
 
-        let negociacao = new Negociacao(
-            data,
+        console.log(this._listaNegociacoes.negociacoes);
+	}
+
+    _limpaFormulario(){
+        this._inputData = "";
+        this._inputData.focus();
+        this._inputQuantidade = 1;
+        this._inputValor=0.0;
+    }
+
+    _criaNegociacao(){
+        return new Negociacao(
+            DateHelper.textoParaData(this._inputData.value),
             this._inputQuantidade.value,
             this._inputValor.value
         );
-
-        this._listaNegociacoes.adiciona(negociacao);
-        
-        console.log(this._listaNegociacoes.negociacoes);
-	}
+    }
 }
