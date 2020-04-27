0227b4b845a3a5bf45a3f96a44908e06ccc2f9b2
Fernando Figueiredo Drummond Bittencourt <fernandodbittencourt@hotmail.com>
2020-04-27 20:56:38 -0300

    Usando Promise no POST.

--- client/js/app/services/HttpService.js
+++ client/js/app/services/HttpService.js
@@ -1,6 +1,7 @@
 class HttpService {
 
     get(url) {
+
         return new Promise((resolve, reject) => {
             let xhr = new XMLHttpRequest();
 
@@ -18,5 +19,28 @@ class HttpService {
 
             xhr.send();
         });
+    }
+
+    post(url, dado) {
+
+        return new Promise((resolve, reject) => {
+
+            let xhr = new XMLHttpRequest();
+
+            xhr.open("POST", url, true);
+            xhr.setRequestHeader("Content-type", "application/json");
+
+            xhr.onreadystatechange = () => {
+                if (xhr.readyState == 4) {
+                    if (xhr.status == 200) {
+                        resolve(JSON.parse(xhr.responseText));
+                    } else {
+                        reject(xhr.responseText);
+                    }
+                }
+            };
+            xhr.send(JSON.stringify(dado));
+        });
+
     }
 }
